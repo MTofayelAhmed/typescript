82c43bb4dbec162b4232e9503844c6e1a3e03046
MTofayelAhmed <tofayelahmed324@gmail.com>
2024-07-25 00:37:15 +0200

    done with reference type , literal type

---
+++ module-1/src/1.5.ts
@@ -0,0 +1,17 @@
+// Reference type object
+
+let user : {
+    readonly  CompanyName: string;
+    FirstName: string;
+    MiddleName?: string;
+    LastName: string;
+    isMarried: boolean;
+
+}= {
+    CompanyName: "Programming Hero",
+    FirstName: "Md",
+    MiddleName: "Tofayel",
+    LastName:"Ahmed",
+    isMarried: false
+
+}
