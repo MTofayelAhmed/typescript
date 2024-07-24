75e72ce68628590a734c4e9324c189f23a21c231
MTofayelAhmed <tofayelahmed324@gmail.com>
2024-07-25 01:11:11 +0200

    done with noraml and arrow function

---
+++ module-1/src/1.6.ts
@@ -0,0 +1,21 @@
+function Add (num1: number, num2: number ): number {
+    return num1 + num2
+}
+
+let TotalSum = Add(2, 4)
+console.log(TotalSum)
+
+
+const arrSum = (num1: number, num2:number): number => num1 + num2;
+console.log(arrSum)
+
+
+// function => object => method 
+
+let User = {
+name : "Tofayel",
+balance: 0,
+addBalance(balance: number ): string {
+    return `My new Balance is ${this.balance + balance}`
+}
+}
