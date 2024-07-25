45353d13196c3b11f39d13e1f1705d2950c7e924
MTofayelAhmed <tofayelahmed324@gmail.com>
2024-07-25 02:29:36 +0200

    done with destructuring

---
+++ module-1/src/1.8.ts
@@ -0,0 +1,26 @@
+{
+  const user = {
+    id: 1234,
+    Name: {
+      FirstName: "Md",
+      MiddleName: "Tofayel",
+      LastName: "Ahmed",
+    },
+    Phone: "01857348927",
+    address: "Uganda",
+  };
+
+
+
+  const {id, Phone, Name:{MiddleName} }= user
+
+
+//   array Destructuring 
+const myFriends: string[] = ["rachel", "chandler", "Monica", "Phebe"]
+
+const [,,BestFriend]=myFriends
+
+}
+
+
+
