0c478e7a0ff3b6dea95e9b5d559edf0ce5ee5ca6
MTofayelAhmed <tofayelahmed324@gmail.com>
2024-07-25 16:37:04 +0200

    Done with Union and intersection type

--- module-1/src/1.10.ts
+++ module-1/src/1.10.ts
@@ -2,29 +2,51 @@
 
 // Union Type 
 
-type FrontEndDeveloper = "JuniorDeveloper" | "SeniorDeveloper";
+// type FrontEndDeveloper = "JuniorDeveloper" | "SeniorDeveloper";
 
 
-const newDeveloper: FrontEndDeveloper = "JuniorDeveloper"
+// const newDeveloper: FrontEndDeveloper = "JuniorDeveloper"
 
 
-type User = {
-    Name : string ;
-    Email ?: string;
-    Gender : "Male"| "Female";
-    BloodGroup: "A+"| "B+"| "C+"| "OB+"
+// type User = {
+//     Name : string ;
+//     Email ?: string;
+//     Gender : "Male"| "Female";
+//     BloodGroup: "A+"| "B+"| "C+"| "OB+"
+// }
+
+
+// const user : User = {
+//     Name : "John",
+//     Gender: "Male",
+//     BloodGroup: "OB+"
+
+
+// }
+
+
+// Intersection Type 
+
+
+type FrontEndDeveloper = {
+    Skills : string[];
+    designation : string;
+
+}
+type BackEndDeveloper = {
+    Skills : string[];
+    designation1: string ;
+
 }
 
+type FullStackDeveloper = FrontEndDeveloper & BackEndDeveloper;
 
-const user : User = {
-    Name : "John",
-    Gender: "Male",
-    BloodGroup: "OB+"
-
+const fullStackDeveloper: FullStackDeveloper = {
+    Skills: ["HTML", "CSS", "JavaScript"],
+    designation: "frontendDeveloper",
+    designation1: "backendDeveloper"
 
 }
-
-
 
 
 
