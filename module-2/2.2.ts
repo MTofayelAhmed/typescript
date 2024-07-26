ecfefb9070b53e20201e1fe2f4ac09add983c846
MTofayelAhmed <tofayelahmed324@gmail.com>
2024-07-26 13:15:16 +0200

    done with interface

--- module-2/2.2.ts
+++ module-2/2.2.ts
@@ -19,7 +19,7 @@ interface USerWithRole1 extends User2{
 type USerWithRole = User1 & {role: string}
 
 
-
+// IN JS Function ===> Array ===> object 
 
 
 
@@ -29,10 +29,23 @@ const user1: USerWithRole1 = {
     role: "admin"
 }
 
+type Roll1 = number[];
 
 
+interface Roll2 {
+    [index : number ] : number
+}
 
 
+const roll1: Roll2 = [1, 2, 3, 4];
+
+type Add1 = (num1: number, num2: number)=> number
+
+interface Add2 {
+    (num1: number, num2: number): number
+}
+
+const add: Add2 = (num1, num2)=> num1 + num2
 
 
 
