f1544c1fe8ee990a5d0b518bc9e74146bb0007db
MTofayelAhmed <tofayelahmed324@gmail.com>
2024-07-28 01:15:35 +0200

    done with generic tuple

---
+++ module-2/2.3.ts
@@ -0,0 +1,43 @@
+{
+  // Generic Type
+
+  type GenericArray<T> = Array<T>;
+
+  // const RollNumbers : number []= [1, 2, 3]
+
+  const RollNumbers: GenericArray<number> = [1, 2, 3];
+
+  // const mentors : string[]= ["Mr. X", "Mr. Y", "Mr. Z"]
+  const mentors: GenericArray<string> = ["Mr. X", "Mr. Y", "Mr. Z"];
+
+  const bollArray: GenericArray<boolean> = [true, false, true];
+
+
+
+  const user: GenericArray<{ name: string; age: number }> = [
+    { name: "Md. Tofayel", age: 32 },
+    {
+      name: "Md. Shahjalal",
+      age: 32,
+    },
+  ];
+
+// Generic Tuple 
+
+type GenericTuple<X, Y >= [X, Y]
+
+const manush : GenericTuple<string, string> = ["Mr. X", "Mr. Y"]
+
+
+const user3 : GenericTuple<number, {name: string, age: string}>= [123, {name: "John", age: "28"}]
+
+
+
+
+
+
+
+
+
+
+}
