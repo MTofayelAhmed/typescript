e42db2e9afc2d998547e3b43e6399ad813a026b2
MTofayelAhmed <tofayelahmed324@gmail.com>
2024-07-28 12:56:58 +0200

    done with 2.5

--- module-2/2.5.ts
+++ module-2/2.5.ts
@@ -44,6 +44,8 @@ type USerS={
 const courseStudent1 = AddCourseToStudent<USerS>({name: "Tofayel Ahmed", id:2222, email: "X@gmail.com"})
 
 
+type User2 = {name: string, id: number, hasWatch: string}
+const courseStudent2 = AddCourseToStudent<User2>({name: "Shahjalal", id: 3333, hasWatch: "Apple"})
 
 
 
