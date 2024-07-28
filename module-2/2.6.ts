88ab21be7b386d807b75cfbd81524c9b2549e12c
MTofayelAhmed <tofayelahmed324@gmail.com>
2024-07-28 13:05:19 +0200

    done with constraints

--- module-2/2.6.ts
+++ module-2/2.6.ts
@@ -1,14 +1,30 @@
 {
 
 
-
-
-
-
-
-
-
-
-
+    const AddCourseToStudent =<T extends {id: number, name: string, email: string}> (student: T)=> {
+        const course = "Next Level Web Development"
+        return {
+            ...student,
+            course
+        }
+    }
+    type USerS={
+        name: string, 
+        id: number,
+        email: string
+    }
+    const courseStudent1 = AddCourseToStudent<USerS>({name: "Tofayel Ahmed", id:2222, email: "X@gmail.com"})
     
+    
+    type User2 = {name: string, id: number, hasWatch: string}
+    const courseStudent2 = AddCourseToStudent<User2>({name: "Shahjalal", id: 3333, hasWatch: "Apple"})
+
+
+
+
+
+
+
+
+
 }
