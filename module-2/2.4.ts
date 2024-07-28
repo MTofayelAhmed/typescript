4ab0a3a2b22b20752c7c6f2a26aaac8b2cd8339f
MTofayelAhmed <tofayelahmed324@gmail.com>
2024-07-28 02:01:44 +0200

    Done with generic interface

--- module-2/2.4.ts
+++ module-2/2.4.ts
@@ -31,6 +31,29 @@
     },
   };
 
+  type AppleWatch = {
+    brand: string,
+      model: string,
+      display: string,
+      sleepTrack: boolean,
+      heartTrack: boolean
+  }
+
+  const SmartDeveloper: Developer<AppleWatch> = {
+    name: " Bhai",
+    computer: {
+      brand: "Apple",
+      model: "XXXXXX",
+      releaseYear: 2022,
+    },
+    smartWatch: {
+      brand: "Apple",
+      model: "XXXYYY",
+      display: "OLED",
+      sleepTrack: true,
+      heartTrack: true
+    },
+  };
 
 
 
@@ -47,5 +70,6 @@
 
 
 
-  
+
+
 }
