2dc80b6327860d1b5e776635108c195a9ccf0bc6
MTofayelAhmed <tofayelahmed324@gmail.com>
2024-07-29 01:14:22 +0200

    done with typeScript

--- module-2/2.9.ts
+++ module-2/2.9.ts
@@ -12,6 +12,19 @@ type c = a1 extends number ? true : false ;
 type d = a1 extends null ? true : a2 extends string ? true : false;
 
 
+type Sheikh = {
+    bike: string;
+    car: string;
+    ship: string;
+}
+
+
+type CheckVehicle<T> = T extends keyof Sheikh ? true : false;
+
+
+type hasShip = CheckVehicle<"ship">
+
+
 
 
 
