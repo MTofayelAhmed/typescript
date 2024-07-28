e670746fefd8492c929d32cf51dfba7c2c177ba5
MTofayelAhmed <tofayelahmed324@gmail.com>
2024-07-28 12:39:37 +0200

    done with genric with function

--- module-2/2.5.ts
+++ module-2/2.5.ts
@@ -18,10 +18,16 @@ type User = {name: string, id: number}
 const resWithGeneric = createArrayWithGeneric<string>("Bangladesh")
 const resWithObj= createArrayWithGeneric<User>({name: "Bangladesh", id: 222})
 
+// create Generic with tuple 
+const createGenericWithTuple =<T, Q> (params1: T,params2: Q):[T, Q] => {
+    return [params1, params2]
+}
 
 
 
+type User1 = {id: number, name: string}
 
+const restTuple = createGenericWithTuple<string,User1 >("Bangladesh", {id: 222, name: "Tofayel"})
 
 
 
