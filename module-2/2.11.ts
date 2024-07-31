0d884e648f7bf3921fa4a5cf41308293abf5a84b
MTofayelAhmed <tofayelahmed324@gmail.com>
2024-08-01 00:54:30 +0200

    done with readonly property

--- module-2/2.11.ts
+++ module-2/2.11.ts
@@ -19,8 +19,20 @@ type ContactInfo = Omit <Person, "name"| "age">
 // required utility 
 type PersonRequired = Required<Person>
 
+// Partial Utility 
 
+type PersonPartial = Partial<Person>
 
+// Readonly Type 
+
+type PersonReadOnly = Readonly<Person>
+
+const personInfo: PersonReadOnly = {
+    name: "Mr. X",
+    age: 39,
+    email: "stahmed@gmail.com",
+    contact: "017"
+}
 
 
 
