2190a23e7dbc03b769ccd49fd211ebf42b6dd65a
MTofayelAhmed <tofayelahmed324@gmail.com>
2024-07-31 23:57:49 +0200

    done with map type

---
+++ module-2/2.10.ts
@@ -0,0 +1,18 @@
+{
+
+
+const arrayNumber: number[]= [1, 2, 3]
+
+const arrayString: string[]= arrayNumber.map(number => number.toString())
+
+console.log(arrayString)
+
+
+type Area = {
+    height: number;
+    width: number;
+}
+
+
+
+}
