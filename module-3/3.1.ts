19df67f44ebcdeba64d5be43e94f013af45e8696
MTofayelAhmed <tofayelahmed324@gmail.com>
2024-08-01 02:11:24 +0200

    done with class and constructor concepts

---
+++ module-3/3.1.ts
@@ -0,0 +1,27 @@
+{
+
+    class Animal {
+        name: string;
+        species: string;
+        sound: string;
+
+
+        constructor(name: string, species: string, sound: string){
+            this.name = name;
+            this.species= species;
+            this.sound = sound
+        }
+
+        
+    }
+
+
+
+
+
+
+
+
+
+
+}
